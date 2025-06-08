f1d9404e1389f58cea939eefebb49b036b0891aa
adiett <paadit041@gmail.com>
2025-06-09 03:48:31 +0700

    Create group.js

---
+++ cmd/group.js
@@ -0,0 +1,37 @@
+/*
+ * -----------------------------------------------------------------------------
+ *  Created by    : ditss
+ *  GitHub        : https://github.com/ditss-dev
+ *  WhatsApp      : https://wa.me/6281513607731
+ *  Description   : Source code project Asuma Gen 2 - WhatsApp Bot
+ *  Created Date  : 2025.
+ * -----------------------------------------------------------------------------
+ *  Feel free to use and modify this script.
+ *  Please keep the header intact when distributing.
+ * -----------------------------------------------------------------------------*/
+module.exports = (prefix) => {
+  return `
+> ┌  ◦ *\`GROUP MENU\`* :
+> ├  ◦ *\`${prefix}add (62xxx)\`*
+> ├  ◦ *\`${prefix}delete (reply pesan)\`*
+> ├  ◦ *\`${prefix}demote (@tag/62xxx)\`*
+> ├  ◦ *\`${prefix}group (khusus admin)\`*
+> ├  ◦ *\`${prefix}group set\`*
+> ├  ◦ *\`${prefix}hidetag\`*
+> ├  ◦ *\`${prefix}kick (@tag/62xxx)\`*
+> ├  ◦ *\`${prefix}linkgrup\`*
+> ├  ◦ *\`${prefix}pin\`*
+> ├  ◦ *\`${prefix}promote (@tag/62xxx)\`*
+> ├  ◦ *\`${prefix}revoke\`*
+> ├  ◦ *\`${prefix}setdesc (desk)\`*
+> ├  ◦ *\`${prefix}setname (nama baru gc)\`*
+> ├  ◦ *\`${prefix}setppgc (reply imgnya)\`*
+> ├  ◦ *\`${prefix}tagall\`*
+> ├  ◦ *\`${prefix}totag (reply pesan)\`*
+> ├  ◦ *\`${prefix}unpin\`*
+> ├  ◦ *\`${prefix}unwarn (@tag/62xxx)\`*
+> ├  ◦ *\`${prefix}warn (@tag/62xxx)\`*
+> ├  ◦ *\`${prefix}listonline\`*
+> └  ◦ *\`${prefix}group set\`*
+`
+}
