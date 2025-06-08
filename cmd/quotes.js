40d4cce7fb81b0b50e3df616150bc0e6fc99c750
adiett <paadit041@gmail.com>
2025-06-09 03:49:38 +0700

    Create quotes.js

---
+++ cmd/quotes.js
@@ -0,0 +1,23 @@
+/*
+ * -----------------------------------------------------------------------------
+ *  Created by    : ditss
+ *  GitHub        : https://github.com/ditss-dev
+ *  WhatsApp      : https://wa.me/6281513607731
+ *  Description   : Source code project Asuma Gen 2 - WhatsApp Bot
+ *  Created Date  : 2025.
+ * -----------------------------------------------------------------------------
+ *  Feel free to use and modify this script.
+ *  Please keep the header intact when distributing.
+ * -----------------------------------------------------------------------------*/
+module.exports = (prefix) => {
+  return `
+> ┌  ◦ *\`QUOTES MENU\`* :
+> ├  ◦ *\`${prefix}bijak\`* :
+> ├  ◦ *\`${prefix}bucin\`* :
+> ├  ◦ *\`${prefix}dare\`* :
+> ├  ◦ *\`${prefix}motivasi\`* :
+> ├  ◦ *\`${prefix}quotes\`* :
+> ├  ◦ *\`${prefix}renungan\`* :
+> └  ◦ *\`${prefix}truth\`* :
+`
+}
