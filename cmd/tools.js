3bcabdfdf4ba8174ff8baaa75a73eb644ef92b6d
adiett <paadit041@gmail.com>
2025-06-09 03:51:17 +0700

    Create tools.js

---
+++ cmd/tools.js
@@ -0,0 +1,59 @@
+/*
+ * -----------------------------------------------------------------------------
+ *  Created by    : ditss
+ *  GitHub        : https://github.com/ditss-dev
+ *  WhatsApp      : https://wa.me/6281513607731
+ *  Description   : Source code project Asuma Gen 2 - WhatsApp Bot
+ *  Created Date  : 2025.
+ * -----------------------------------------------------------------------------
+ *  Feel free to use and modify this script.
+ *  Please keep the header intact when distributing.
+ * -----------------------------------------------------------------------------*/
+module.exports = (prefix) => {
+  return `
+> ┌  ◦ *\`TOOLS MENU\`* :
+> ├  ◦ *\`${prefix}get (url)\`* :
+> ├  ◦ *\`${prefix}hd (reply pesan)\`* :
+> ├  ◦ *\`${prefix}removebg (reply pesan)\`* :
+> ├  ◦ *\`${prefix}toaudio (reply pesan)\`* :
+> ├  ◦ *\`${prefix}tomp3 (reply pesan)\`* :
+> ├  ◦ *\`${prefix}tovn (reply pesan)\`* :
+> ├  ◦ *\`${prefix}toimage (reply pesan)\`* :
+> ├  ◦ *\`${prefix}toptv (reply pesan)\`* :
+> ├  ◦ *\`${prefix}tourl (reply pesan)\`* :
+> ├  ◦ *\`${prefix}tts (textnya)\`* :
+> ├  ◦ *\`${prefix}toqr (textnya)\`* :
+> ├  ◦ *\`${prefix}brat (textnya)\`* :
+> ├  ◦ *\`${prefix}bratvid (textnya)\`* :
+> ├  ◦ *\`${prefix}ssweb (url)\`* :
+> ├  ◦ *\`${prefix}sticker (send/reply img)\`* :
+> ├  ◦ *\`${prefix}colong (reply stiker)\`* :
+> ├  ◦ *\`${prefix}smeme (send/reply img)\`* :
+> ├  ◦ *\`${prefix}dehaze (send/reply img)\`* :
+> ├  ◦ *\`${prefix}colorize (send/reply img)\`* :
+> ├  ◦ *\`${prefix}hitamkan (send/reply img)\`* :
+> ├  ◦ *\`${prefix}emojimix 🙃+💀\`* :
+> ├  ◦ *\`${prefix}emojigif 🙃\`* :
+> ├  ◦ *\`${prefix}nulis\`* :
+> ├  ◦ *\`${prefix}readmore text1|text2\`* :
+> ├  ◦ *\`${prefix}qc (pesannya)\`* :
+> ├  ◦ *\`${prefix}translate\`* :
+> ├  ◦ *\`${prefix}wasted (send/reply img)\`* :
+> ├  ◦ *\`${prefix}triggered (send/reply img)\`* :
+> ├  ◦ *\`${prefix}shorturl (urlnya)\`* :
+> ├  ◦ *\`${prefix}gitclone (urlnya)\`* :
+> ├  ◦ *\`${prefix}fat (reply audio)\`* :
+> ├  ◦ *\`${prefix}fast (reply audio)\`* :
+> ├  ◦ *\`${prefix}bass (reply audio)\`* :
+> ├  ◦ *\`${prefix}slow (reply audio)\`* :
+> ├  ◦ *\`${prefix}tupai (reply audio)\`* :
+> ├  ◦ *\`${prefix}deep (reply audio)\`* :
+> ├  ◦ *\`${prefix}robot (reply audio)\`* :
+> ├  ◦ *\`${prefix}blown (reply audio)\`* :
+> ├  ◦ *\`${prefix}reverse (reply audio)\`* :
+> ├  ◦ *\`${prefix}smooth (reply audio)\`* :
+> ├  ◦ *\`${prefix}earrape (reply audio)\`* :
+> ├  ◦ *\`${prefix}nightcore (reply audio)\`* :
+> └  ◦ *\`${prefix}getexif (reply sticker)\`* :
+`
+}
