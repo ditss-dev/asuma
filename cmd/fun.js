f1c16c12f5ddcf201adf8655718ba8438f090f1d
adiett <paadit041@gmail.com>
2025-06-09 03:47:27 +0700

    Create fun.js

---
+++ cmd/fun.js
@@ -0,0 +1,88 @@
+/*
+ * -----------------------------------------------------------------------------
+ *  Created by    : ditss
+ *  GitHub        : https://github.com/ditss-dev
+ *  WhatsApp      : https://wa.me/6281513607731
+ *  Description   : Source code project Asuma Gen 2 - WhatsApp Bot
+ *  Created Date  : 2025.
+ * -----------------------------------------------------------------------------
+ *  Feel free to use and modify this script.
+ *  Please keep the header intact when distributing.
+ * -----------------------------------------------------------------------------*/
+module.exports = (prefix) => {
+  return `
+> ┌  ◦ *\`FUN MENU\`* :
+> ├  ◦ *\`${prefix}anj\`* :
+> ├  ◦ *\`${prefix}anjing\`* :
+> ├  ◦ *\`${prefix}apakah (text)\`* :
+> ├  ◦ *\`${prefix}asu\`* :
+> ├  ◦ *\`${prefix}babi\`* :
+> ├  ◦ *\`${prefix}bajingan\`* :
+> ├  ◦ *\`${prefix}bangke\`* :
+> ├  ◦ *\`${prefix}bangsat\`* :
+> ├  ◦ *\`${prefix}bego\`* :
+> ├  ◦ *\`${prefix}bisakah (text)\`* :
+> ├  ◦ *\`${prefix}bodoh\`* :
+> ├  ◦ *\`${prefix}cacat\`* :
+> ├  ◦ *\`${prefix}cantik\`* :
+> ├  ◦ *\`${prefix}cekkhodam (nama lu)\`* :
+> ├  ◦ *\`${prefix}cekmati (nama lu)\`* :
+> ├  ◦ *\`${prefix}ceksifat\`* :
+> ├  ◦ *\`${prefix}china\`* :
+> ├  ◦ *\`${prefix}chindo\`* :
+> ├  ◦ *\`${prefix}cina\`* :
+> ├  ◦ *\`${prefix}coba\`* :
+> ├  ◦ *\`${prefix}dakjal\`* :
+> ├  ◦ *\`${prefix}dadu\`* :
+> ├  ◦ *\`${prefix}dongp\`* :
+> ├  ◦ *\`${prefix}duda\`* :
+> ├  ◦ *\`${prefix}fitnah\`* :
+> ├  ◦ *\`${prefix}ganteng\`* :
+> ├  ◦ *\`${prefix}gay\`* :
+> ├  ◦ *\`${prefix}goblok\`* :
+> ├  ◦ *\`${prefix}halah (text)\`* :
+> ├  ◦ *\`${prefix}heleh (text)\`* :
+> ├  ◦ *\`${prefix}hilih (text)\`* :
+> ├  ◦ *\`${prefix}holoh (text)\`* :
+> ├  ◦ *\`${prefix}horny\`* :
+> ├  ◦ *\`${prefix}huluh (text)\`* :
+> ├  ◦ *\`${prefix}iblis\`* :
+> ├  ◦ *\`${prefix}jadian\`* :
+> ├  ◦ *\`${prefix}janda\`* :
+> ├  ◦ *\`${prefix}jancok\`* :
+> ├  ◦ *\`${prefix}jawa\`* :
+> ├  ◦ *\`${prefix}jodohku\`* :
+> ├  ◦ *\`${prefix}kapan (text)\`* :
+> ├  ◦ *\`${prefix}kerangajaib (text)\`* :
+> ├  ◦ *\`${prefix}lesbi\`* :
+> ├  ◦ *\`${prefix}lesby\`* :
+> ├  ◦ *\`${prefix}mastah\`* :
+> ├  ◦ *\`${prefix}monyet\`* :
+> ├  ◦ *\`${prefix}newbie\`* :
+> ├  ◦ *\`${prefix}ngentod\`* :
+> ├  ◦ *\`${prefix}ngentot\`* :
+> ├  ◦ *\`${prefix}nolep\`* :
+> ├  ◦ *\`${prefix}papua\`* :
+> ├  ◦ *\`${prefix}pantek\`* :
+> ├  ◦ *\`${prefix}pantex\`* :
+> ├  ◦ *\`${prefix}peak\`* :
+> ├  ◦ *\`${prefix}pedo\`* :
+> ├  ◦ *\`${prefix}perawan\`* :
+> ├  ◦ *\`${prefix}piatu\`* :
+> ├  ◦ *\`${prefix}pinter\`* :
+> ├  ◦ *\`${prefix}pintar\`* :
+> ├  ◦ *\`${prefix}puki\`* :
+> ├  ◦ *\`${prefix}rate (reply pesan)\`* :
+> ├  ◦ *\`${prefix}sampah\`* :
+> ├  ◦ *\`${prefix}sange\`* :
+> ├  ◦ *\`${prefix}sangean\`* :
+> ├  ◦ *\`${prefix}setan\`* :
+> ├  ◦ *\`${prefix}siapa (text)\`* :
+> ├  ◦ *\`${prefix}sunda\`* :
+> ├  ◦ *\`${prefix}tolol\`* :
+> ├  ◦ *\`${prefix}wibu\`* :
+> ├  ◦ *\`${prefix}yapit\`* :
+> ├  ◦ *\`${prefix}yatim\`* :
+> └  ◦ *Fun zone by Asuma.!*
+`
+}
