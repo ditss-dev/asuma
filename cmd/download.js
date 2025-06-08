3939400508b277747f0e2a067f849920af7040ef
adiett <paadit041@gmail.com>
2025-06-09 03:46:43 +0700

    Create download.js

---
+++ cmd/download.js
@@ -0,0 +1,28 @@
+/*
+ * -----------------------------------------------------------------------------
+ *  Created by    : ditss
+ *  GitHub        : https://github.com/ditss-dev
+ *  WhatsApp      : https://wa.me/6281513607731
+ *  Description   : Source code project Asuma Gen 2 - WhatsApp Bot
+ *  Created Date  : 2025.
+ * -----------------------------------------------------------------------------
+ *  Feel free to use and modify this script.
+ *  Please keep the header intact when distributing.
+ * -----------------------------------------------------------------------------*/
+module.exports = (prefix) => {
+  return `
+> ┌  ◦ *\`DOWNLOAD MENU\`* :
+> ├  ◦ *\`${prefix}capcutdl (url)\`*
+> ├  ◦ *\`${prefix}facebook (url)\`*
+> ├  ◦ *\`${prefix}gitclone (url)\`*
+> ├  ◦ *\`${prefix}happymod (url)\`*
+> ├  ◦ *\`${prefix}instagram (url)\`*
+> ├  ◦ *\`${prefix}mediafire (url)\`*
+> ├  ◦ *\`${prefix}playstore (url)\`*
+> ├  ◦ *\`${prefix}spotifydl (url)\`*
+> ├  ◦ *\`${prefix}tiktok (url)\`*
+> ├  ◦ *\`${prefix}tiktokmp3 (url)\`*
+> ├  ◦ *\`${prefix}ytmp3 (url)\`*
+> └  ◦ *\`${prefix}ytmp4 (url)\`*
+`
+}
