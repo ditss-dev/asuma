266ce40847f7e907e02ab4b9ea5a13ef9fa5b6fd
adiett <paadit041@gmail.com>
2025-06-09 03:47:49 +0700

    Create game.js

---
+++ cmd/game.js
@@ -0,0 +1,41 @@
+/*
+ * -----------------------------------------------------------------------------
+ *  Created by    : ditss
+ *  GitHub        : https://github.com/ditss-dev
+ *  WhatsApp      : https://wa.me/6281513607731
+ *  Description   : Source code project Asuma Gen 2 - WhatsApp Bot
+ *  Created Date  : 2025.
+ * -----------------------------------------------------------------------------
+ *  Feel free to use and modify this script.
+ *  Please keep the header intact when distributing.
+ * -----------------------------------------------------------------------------*/
+module.exports = (prefix) => {
+  return `
+> ┌  ◦ *\`GAME MENU\`* :
+> ├  ◦ *\`${prefix}tictactoe\`* :
+> ├  ◦ *\`${prefix}akinator\`* :
+> ├  ◦ *\`${prefix}suit\`* :
+> ├  ◦ *\`${prefix}slot\`* :
+> ├  ◦ *\`${prefix}math (level)\`* :
+> ├  ◦ *\`${prefix}begal\`* :
+> ├  ◦ *\`${prefix}ulartangga\`* :
+> ├  ◦ *\`${prefix}blackjack\`* :
+> ├  ◦ *\`${prefix}catur\`* :
+> ├  ◦ *\`${prefix}casino (nominal)\`* :
+> ├  ◦ *\`${prefix}samgong (nominal)\`* :
+> ├  ◦ *\`${prefix}rampok (@tag)\`* :
+> ├  ◦ *\`${prefix}tekateki\`* :
+> ├  ◦ *\`${prefix}tebaklirik\`* :
+> ├  ◦ *\`${prefix}tebakkata\`* :
+> ├  ◦ *\`${prefix}tebakbom\`* :
+> ├  ◦ *\`${prefix}susunkata\`* :
+> ├  ◦ *\`${prefix}colorblind\`* :
+> ├  ◦ *\`${prefix}tebakkimia\`* :
+> ├  ◦ *\`${prefix}caklontong\`* :
+> ├  ◦ *\`${prefix}tebakangka\`* :
+> ├  ◦ *\`${prefix}tebaknegara\`* :
+> ├  ◦ *\`${prefix}tebakgambar\`* :
+> ├  ◦ *\`${prefix}tebakbendera\`* :
+> └  ◦ *Gamer mode by Ditss!*
+`
+}
