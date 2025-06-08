ebd320e7296792530eb2e3dd23097e85ab765022
adiett <paadit041@gmail.com>
2025-06-09 03:45:40 +0700

    Create anime.js

---
+++ cmd/anime.js
@@ -0,0 +1,24 @@
+/*
+ * -----------------------------------------------------------------------------
+ *  Created by    : ditss
+ *  GitHub        : https://github.com/ditss-dev
+ *  WhatsApp      : https://wa.me/6281513607731
+ *  Description   : Source code project Asuma Gen 2 - WhatsApp Bot
+ *  Created Date  : 2025.
+ * -----------------------------------------------------------------------------
+ *  Feel free to use and modify this script.
+ *  Please keep the header intact when distributing.
+ * -----------------------------------------------------------------------------
+ */
+module.exports = (prefix) => {
+  return `
+> ┌  ◦ *\`ANIME MENU\`* :
+> ├  ◦ *\`${prefix}waifu\`*  - Tampilkan gambar waifu random
+> ├  ◦ *\`${prefix}neko\`*   - Tampilkan gambar neko imut
+> ├  ◦ *\`${prefix}loli\`*   - Gambar loli random
+> ├  ◦ *\`${prefix}megumin\`* - Gambar Megumin keren
+> ├  ◦ *\`${prefix}shinobu\`* - Gambar Shinobu manis
+> ├  ◦ *\`${prefix}tanjiro-kamado\`* - Gambar Tanjiro Kamado semangat
+> └  ◦ *\`${prefix}gojo\`*    - Gambar Satoru Gojo keren
+`
+}
