8eebcb3090095e1ffd466e60b5c8b7487cca2265
adiett <paadit041@gmail.com>
2025-06-09 03:46:04 +0700

    Create bot.js

---
+++ cmd/bot.js
@@ -0,0 +1,46 @@
+/*
+ * -----------------------------------------------------------------------------
+ *  Created by    : ditss
+ *  GitHub        : https://github.com/ditss-dev
+ *  WhatsApp      : https://wa.me/6281513607731
+ *  Description   : Source code project Asuma Gen 2 - WhatsApp Bot
+ *  Created Date  : 2025.
+ * -----------------------------------------------------------------------------
+ *  Feel free to use and modify this script.
+ *  Please keep the header intact when distributing.
+ * -----------------------------------------------------------------------------*/
+module.exports = (prefix) => {
+  return `
+> ┌  ◦ *\`BOT MENU\`* :
+> ├  ◦ *\`${prefix}addmsg\`*
+> ├  ◦ *\`${prefix}afk\`*
+> ├  ◦ *\`${prefix}buy [item] (nominal)\`*
+> ├  ◦ *\`${prefix}claim\`*
+> ├  ◦ *\`${prefix}confes (62xxx|fake name)\`*
+> ├  ◦ *\`${prefix}delmsg\`*
+> ├  ◦ *\`${prefix}donasi\`*
+> ├  ◦ *\`${prefix}delsewa\`*
+> ├  ◦ *\`${prefix}getmsg\`*
+> ├  ◦ *\`${prefix}inspect (url gc)\`*
+> ├  ◦ *\`${prefix}jadibot 🔸️\`*
+> ├  ◦ *\`${prefix}leaderboard\`*
+> ├  ◦ *\`${prefix}listjadibot\`*
+> ├  ◦ *\`${prefix}listmsg\`*
+> ├  ◦ *\`${prefix}listsewa\`*
+> ├  ◦ *\`${prefix}menfes (62xxx|fake name)\`*
+> ├  ◦ *\`${prefix}ping\`*
+> ├  ◦ *\`${prefix}profile\`*
+> ├  ◦ *\`${prefix}q (reply pesan)\`*
+> ├  ◦ *\`${prefix}react (emoji)\`*
+> ├  ◦ *\`${prefix}request (text)\`*
+> ├  ◦ *\`${prefix}rvo (reply pesan viewone)\`*
+> ├  ◦ *\`${prefix}runtime\`*
+> ├  ◦ *\`${prefix}speed\`*
+> ├  ◦ *\`${prefix}stopjadibot\`*
+> ├  ◦ *\`${prefix}tagme\`*
+> ├  ◦ *\`${prefix}totalfitur\`*
+> ├  ◦ *\`${prefix}transfer\`*
+> ├  ◦ *\`${prefix}addsewa\`*
+> └  ◦ *\`${prefix}roomai\`*
+`
+}
