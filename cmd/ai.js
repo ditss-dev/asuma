cc1e3c72d634989c933c91b13cf0cb16c9fd79da
adiett <paadit041@gmail.com>
2025-06-09 03:45:18 +0700

    Create ai.js

---
+++ cmd/ai.js
@@ -0,0 +1,21 @@
+/*
+ * -----------------------------------------------------------------------------
+ *  Created by    : ditss
+ *  GitHub        : https://github.com/ditss-dev
+ *  WhatsApp      : https://wa.me/6281513607731
+ *  Description   : Source code project Asuma Gen 2 - WhatsApp Bot
+ *  Created Date  : 2025.
+ * -----------------------------------------------------------------------------
+ *  Feel free to use and modify this script.
+ *  Please keep the header intact when distributing.
+ * -----------------------------------------------------------------------------
+ */
+module.exports = (prefix) => {
+  return `
+> ┌  ◦ *\`AI MENU\`* :
+> ├  ◦ *\`${prefix}ai (query)\`* :
+> ├  ◦ *\`${prefix}simi (query)\`* :
+> ├  ◦ *\`${prefix}gemini (query)\`* :
+> └  ◦ *\`${prefix}txt2img (query)\`* :
+`
+}
