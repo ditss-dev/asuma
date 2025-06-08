c12445e9be46e8a5967154615fb2affdd667cefd
adiett <paadit041@gmail.com>
2025-06-09 03:50:30 +0700

    Create search.js

---
+++ cmd/search.js
@@ -0,0 +1,31 @@
+/*
+ * -----------------------------------------------------------------------------
+ *  Created by    : ditss
+ *  GitHub        : https://github.com/ditss-dev
+ *  WhatsApp      : https://wa.me/6281513607731
+ *  Description   : Source code project Asuma Gen 2 - WhatsApp Bot
+ *  Created Date  : 2025.
+ * -----------------------------------------------------------------------------
+ *  Feel free to use and modify this script.
+ *  Please keep the header intact when distributing.
+ * -----------------------------------------------------------------------------*/
+module.exports = (prefix) => {
+  return `
+> ┌ ◦ *\`SEARCH MENU\`* :
+> ├ ◦ *\`${prefix}cuaca (kota)\`*
+> ├ ◦ *\`${prefix}gimage (query)\`*
+> ├ ◦ *\`${prefix}google (query)\`*
+> ├ ◦ *\`${prefix}npm (query)\`*
+> ├ ◦ *\`${prefix}pinterest (query)\`*
+> ├ ◦ *\`${prefix}pixiv (query)\`*
+> ├ ◦ *\`${prefix}ringtone (query)\`*
+> ├ ◦ *\`${prefix}happymod (query)\`*
+> ├ ◦ *\`${prefix}spotify (query)\`*
+> ├ ◦ *\`${prefix}playstore (query)\`*
+> ├ ◦ *\`${prefix}style (query)\`*
+> ├ ◦ *\`${prefix}tenor (query)\`*
+> ├ ◦ *\`${prefix}urban (query)\`*
+> ├ ◦ *\`${prefix}wallpaper (query)\`*
+> └ ◦ *\`${prefix}ytsearch (query)\`*
+`
+}
