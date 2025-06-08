62ccc7df106461a1abd90efc6a1cf1ac5fcb9366
adiett <paadit041@gmail.com>
2025-06-09 03:49:14 +0700

    Create owner.js

---
+++ cmd/owner.js
@@ -0,0 +1,48 @@
+/*
+ * -----------------------------------------------------------------------------
+ *  Created by    : ditss
+ *  GitHub        : https://github.com/ditss-dev
+ *  WhatsApp      : https://wa.me/6281513607731
+ *  Description   : Source code project Asuma Gen 2 - WhatsApp Bot
+ *  Created Date  : 2025.
+ * -----------------------------------------------------------------------------
+ *  Feel free to use and modify this script.
+ *  Please keep the header intact when distributing.
+ * -----------------------------------------------------------------------------*/
+module.exports = (prefix) => {
+  return `
+     🎖️ *OWNER MENU* 🎖️
+> ┌  ◦ *\`${prefix}bot [set]\`* :
+> ├  ◦ *\`${prefix}setbio\`* :
+> ├  ◦ *\`${prefix}setppbot\`* :
+> ├  ◦ *\`${prefix}join\`* :
+> ├  ◦ *\`${prefix}leave\`* :
+> ├  ◦ *\`${prefix}block\`* :
+> ├  ◦ *\`${prefix}listblock\`* :
+> ├  ◦ *\`${prefix}openblock\`* :
+> ├  ◦ *\`${prefix}listpc\`* :
+> ├  ◦ *\`${prefix}listgc\`* :
+> ├  ◦ *\`${prefix}ban\`* :
+> ├  ◦ *\`${prefix}unban\`* :
+> ├  ◦ *\`${prefix}mute\`* :
+> ├  ◦ *\`${prefix}unmute\`* :
+> ├  ◦ *\`${prefix}creategc\`* :
+> ├  ◦ *\`${prefix}clearchat\`* :
+> ├  ◦ *\`${prefix}addprem\`* :
+> ├  ◦ *\`${prefix}delprem\`* :
+> ├  ◦ *\`${prefix}listprem\`* :
+> ├  ◦ *\`${prefix}addlimit\`* :
+> ├  ◦ *\`${prefix}adduang\`* :
+> ├  ◦ *\`${prefix}getmsgstore\`* :
+> ├  ◦ *\`${prefix}bot --settings\`* :
+> ├  ◦ *\`${prefix}bot settings\`* :
+> ├  ◦ *\`${prefix}getsession\`* :
+> ├  ◦ *\`${prefix}delsession\`* :
+> ├  ◦ *\`${prefix}delsampah\`* :
+> ├  ◦ *\`${prefix}upsw\`* :
+> ├  ◦ *\`${prefix}backup\`* :
+> ├  ◦ *\`$\`* :
+> ├  ◦ *\`>\`* :
+> └  ◦ *\`<\`* :
+`
+}
